9763ab976db743a6ee595a0a94fe864c38ba08f4
Zahara <fathimaf@lolctech.com>
2022-06-23 12:31:09 +0530

    location access

--- app/src/main/java/com/example/location_access/MainActivity.java
+++ app/src/main/java/com/example/location_access/MainActivity.java
@@ -16,28 +16,21 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     Button location_button;
-    //TextView textView, textView2, textView3, textView4, textView5;
     FusedLocationProviderClient fusedLocationProviderClient;
+
 
 
 
@@ -48,11 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         location_button = findViewById(R.id.location_button);
-        //textView = findViewById(R.id.textView);
-        //textView2 = findViewById(R.id.textView2);
-        //textView3 = findViewById(R.id.textView3);
-        //textView4 = findViewById(R.id.textView4);
-        //textView5 = findViewById(R.id.textView5);
+
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         location_button.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +57,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+     */
 
     @SuppressLint("MissingPermission")
     private void getLocation() {
@@ -82,11 +76,6 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
 
-                        //textView.setText(Html.fromHtml("<font color = '#6200EE'><b> Latitude: </b><br></font>" + addresses.get(0).getLatitude()));
-                        //textView2.setText(Html.fromHtml("<font color = '#6200EE'><b> Longitude: </b><br></font>" + addresses.get(0).getLongitude()));
-                        //textView3.setText(Html.fromHtml("<font color = '#6200EE'><b> Country Name: </b><br></font>" + addresses.get(0).getCountryName()));
-                        //textView4.setText(Html.fromHtml("<font color = '#6200EE'><b> Locality: </b><br></font>" + addresses.get(0).getLocality()));
-                        //textView5.setText(Html.fromHtml("<font color = '#6200EE'><b> Address: </b><br></font>" + addresses.get(0).getAddressLine(0)));
 
                         Intent locationIntent = new Intent(MainActivity.this,MainActivity2.class);
                         locationIntent.putExtra("Latitude",addresses.get(0).getLatitude());
@@ -94,8 +83,9 @@ public class MainActivity extends AppCompatActivity {
                         locationIntent.putExtra("CountryName",addresses.get(0).getCountryName());
                         locationIntent.putExtra("Locality",addresses.get(0).getLocality());
                         locationIntent.putExtra("AddressLine",addresses.get(0).getAddressLine(0));
-                        //locationIntent.putExtra("addresses", (Serializable) addresses);
-                        startService(locationIntent);
+                        startActivity(locationIntent);
+
+
 
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -103,9 +93,43 @@ public class MainActivity extends AppCompatActivity {
 
 
                 }
+                else {
+                    //startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                    statusCheck();
+
+                }
+
+
             }
         });
 
+    }
+
+
+    public void statusCheck() {
+        final LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            buildAlertMessageNoGps();
+
+        }
+    }
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, final int id) {
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 
 
--- app/src/main/java/com/example/location_access/MainActivity2.java
+++ app/src/main/java/com/example/location_access/MainActivity2.java
@@ -3,12 +3,10 @@ package com.example.location_access;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.location.Address;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -19,11 +17,11 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
-        textView3 = findViewById(R.id.textView3);
-        textView4 = findViewById(R.id.textView4);
-        textView5 = findViewById(R.id.textView5);
+        textView = findViewById(R.id.textView_1);
+        textView2 = findViewById(R.id.textView_2);
+        textView3 = findViewById(R.id.textView_3);
+        textView4 = findViewById(R.id.textView_4);
+        textView5 = findViewById(R.id.textView_5);
 
         Intent locationGetIntent = getIntent();
         String Latitude = locationGetIntent.getStringExtra("Latitude");
@@ -31,13 +29,13 @@ public class MainActivity2 extends AppCompatActivity {
         String CountryName = locationGetIntent.getStringExtra("CountryName");
         String Locality = locationGetIntent.getStringExtra("Locality");
         String AddressLine = locationGetIntent.getStringExtra("AddressLine");
-        //ArrayList<Address> addresses = locationGetIntent.getParcelableArrayListExtra("addresses");
 
-        textView.setText(Html.fromHtml("<font color = '#6200EE'><b> Latitude: </b><br></font>" + Latitude));
-        textView2.setText(Html.fromHtml("<font color = '#6200EE'><b> Longitude: </b><br></font>" + Longitude));
-        textView3.setText(Html.fromHtml("<font color = '#6200EE'><b> Country Name: </b><br></font>" + CountryName));
-        textView4.setText(Html.fromHtml("<font color = '#6200EE'><b> Locality: </b><br></font>" + Locality));
-        textView5.setText(Html.fromHtml("<font color = '#6200EE'><b> Address: </b><br></font>" + AddressLine));
+        textView.setText(Html.fromHtml("<font color = '#DBE85E'><b> Latitude: </b><br></font>" + Latitude));
+        textView2.setText(Html.fromHtml("<font color = '#DBE85E'><b> Longitude: </b><br></font>" + Longitude));
+        textView3.setText(Html.fromHtml("<font color = '#DBE85E'><b> Country Name: </b><br></font>" + CountryName));
+        textView4.setText(Html.fromHtml("<font color = '#DBE85E'><b> Locality: </b><br></font>" + Locality));
+        textView5.setText(Html.fromHtml("<font color = '#DBE85E'><b> Address: </b><br></font>" + AddressLine));
+
 
 
 
